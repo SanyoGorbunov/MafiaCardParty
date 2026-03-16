9a0dfe47e2e1f8c73bc218b229af3517cef972be
Alexander Gorbunov <sanyo.gorbunov@gmail.com>
2026-03-17 00:52:05 +0100

    Fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
@@ -8,12 +8,17 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
+    exclude:[
+      ...configDefaults.exclude, 
+      'e2e'
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       exclude: [
         'node_modules/',
         'src/test/',
+        'e2e/'
       ],
     },
   },
